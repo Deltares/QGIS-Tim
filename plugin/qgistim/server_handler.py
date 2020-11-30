5faf21c0f19e159dbd78756a0cf7a58c3eb98305
Huite Bootsma <huitebootsma@gmail.com>
2020-11-30 10:21:32 +0100

    black format

--- plugin/qgistim/server_handler.py
+++ plugin/qgistim/server_handler.py
@@ -10,7 +10,7 @@ class ServerHandler:
         self.PORT = None
         self.socket = None
         self.process_ID = None
-    
+
     def find_free_port(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         port = 1024
@@ -22,7 +22,7 @@ class ServerHandler:
             except OSError:
                 port += 1
         else:
-            raise IOError('no free ports')
+            raise IOError("no free ports")
         return port
 
     def start_server(self):
@@ -31,23 +31,23 @@ class ServerHandler:
         configdir = Path(os.environ["APPDATA"]) / "qgis-tim"
         with open(configdir / "interpreter.txt") as f:
             interpreter = f.read().strip()
-        
+
         script = configdir / "activate.py"
         env_vars = configdir / "environmental-variables.json"
-        
+
         process = subprocess.Popen(
-            f"python {script} {env_vars} {interpreter} {self.PORT}", 
-            creationflags=subprocess.CREATE_NEW_CONSOLE
+            f"python {script} {env_vars} {interpreter} {self.PORT}",
+            creationflags=subprocess.CREATE_NEW_CONSOLE,
         )
         self.process_ID = process.pid
-    
+
     def send(self, data):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.HOST, self.PORT))
         self.socket.sendall(bytes(data, "utf-8"))
         received = str(self.socket.recv(1024), "utf-8")
         return received
-    
+
     def kill(self):
         try:
             subprocess.run(["Taskkill", "/PID", self.process_ID, "/F"], check=False)
