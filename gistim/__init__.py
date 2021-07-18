0c372a82f44e04852490d2a8a30235bd0eca12e8
Huite Bootsma <huitebootsma@gmail.com>
2021-07-18 17:52:03 +0200

    Cleanup exported symbols; keep timml and ttim in their own namespace

--- gistim/__init__.py
+++ gistim/__init__.py
@@ -1,21 +1,7 @@
 from .data_extraction import as_aquifer_aquitard, layer_statistics
 from .common import (
     gridspec,
-)
-from .timml_elements import (
-    aquifer,
-    buildingpit,
-    circareasink,
-    constant,
-    headgrid,
-    headwell,
-    implinedoublet,
-    initialize_model,
-    leakylinedoublet,
-    linesinkditch,
     model_specification,
-    polygoninhom,
-    uflow,
-    well,
 )
 from .tim_server import StatefulTimServer, TimHandler
+from gistim import timml_elements, ttim_elements
