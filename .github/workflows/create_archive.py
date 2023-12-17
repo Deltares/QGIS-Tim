710c6a6ee79a72060aac35c16fbe3ed494b45265
Huite Bootsma <huitebootsma@gmail.com>
2023-12-17 21:23:27 +0100

    Print a little info

--- .github/workflows/create_archive.py
+++ .github/workflows/create_archive.py
@@ -7,10 +7,13 @@ import shutil
 # Use the RUNNER_OS variable on the Github Runner. Use platform system locally.
 system = os.environ.get("RUNNER_OS", platform.system())
 zippath = shutil.make_archive(f"dist/gistim-{system}", "zip", "dist/gistim")
+print(f"Created: {zippath}")
 
 # Create a checksum
 with open(zippath, "rb", buffering=0) as f:
     sha256_hash = hashlib.file_digest(f, "sha256").hexdigest()
 
-with open(f"dist/sha256-checksum-{system}.txt", "w") as f:
+txt_path = f"dist/sha256-checksum-{system}.txt"
+print(f"Writing checksum to: {txt_path}")
+with open(txt_path, "w") as f:
     f.write(sha256_hash)
