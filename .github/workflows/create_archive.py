941aa0e8463aeb897a15236f4f1fba88cb434384
Huite Bootsma <huitebootsma@gmail.com>
2023-12-17 21:03:41 +0100

    Multi-OS support

--- .github/workflows/create_archive.py
+++ .github/workflows/create_archive.py
@@ -1,17 +1,16 @@
-from pathlib import Path
 import hashlib
+import os
 import platform
 import shutil
 
 # Create archive
-shutil.make_archive("dist/gistim", "zip", "dist/gistim")
+# Use the RUNNER_OS variable on the Github Runner. Use platform system locally.
+system = os.environ.get("RUNNER_OS", platform.system())
+zippath = shutil.make_archive(f"dist/gistim-{system}", "zip", "dist/gistim")
 
 # Create a checksum
-path = Path("dist/gistim.zip")
-with open(path, "rb", buffering=0) as f:
+with open(zippath, "rb", buffering=0) as f:
     sha256_hash = hashlib.file_digest(f, "sha256").hexdigest()
 
-# Include it in the filename.
-#filename = f"dist/gistim-{platform.system()}-{sha256_hash}.zip"
-filename = f"dist/gistim-{platform.system()}.zip"
-path.rename(filename)
+with open(f"dist/sha256-checksum-{system}.txt", "w") as f:
+    f.write(sha256_hash)
