486e9b70e93ef9494c6bf95b3f805015202cc744
JoerivanEngelen <joerivanengelen@hotmail.com>
2025-11-28 16:36:20 +0100

    Make backwards particle lightgrey, add delt to ttim attributes

--- plugin/qgistim/core/elements/particle.py
+++ plugin/qgistim/core/elements/particle.py
@@ -10,7 +10,7 @@ from qgis.core import (
     QgsLineSymbol,
     QgsSingleSymbolRenderer,
 )
-from qgistim.core.elements.colors import GREY
+from qgistim.core.elements.colors import GREY, LIGHT_GREY
 from qgistim.core.elements.element import TransientElement
 from qgistim.core.elements.schemata import RowWiseSchema
 from qgistim.core.schemata import (
@@ -30,7 +30,6 @@ class ParticleSchema(RowWiseSchema):
     timml_schemata = {
         "geometry": Required(),
         "z_start": Required(),
-        "time_end": Required(Positive()),
         "hstep_max": Required(StrictlyPositive()),
         "vstep_fraction": Required(StrictlyPositive()),
         "nstep_max": Required(StrictlyPositive()),
@@ -51,13 +50,13 @@ class Particle(TransientElement, abc.ABC):
     timml_attributes = (
         QgsField("label", QVariant.String),
         QgsField("z_start", QVariant.Double),
-        QgsField("time_end", QVariant.Double),
         QgsField("hstep_max", QVariant.Double),
         QgsField("vstep_fraction", QVariant.Double),
         QgsField("nstep_max", QVariant.Int),
     )
     ttim_attributes = (
         QgsField("time_start", QVariant.Double),
+        QgsField("delt", QVariant.Double),
         QgsField("time_end", QVariant.Double),
     )
     timml_defaults = {
@@ -65,17 +64,10 @@ class Particle(TransientElement, abc.ABC):
     }
     transient_columns = (
         "time_start",
+        "delt",
         "time_end",
     )
     schema = ParticleSchema()
-
-    @classmethod
-    def renderer(cls) -> QgsSingleSymbolRenderer:
-        return cls.marker_renderer(color=GREY, size="3")
-
-    @classmethod
-    def renderer_output(cls) -> QgsSingleSymbolRenderer:
-        return cls.line_renderer(color=GREY, width="1.0")
 
     def process_timml_row(self, row, other=None) -> Dict[str, Any]:
         x, y = self.point_xy(row)
@@ -85,7 +77,6 @@ class Particle(TransientElement, abc.ABC):
             "zstart": row["z_start"],
             "hstepmax": row["hstep_max"],
             "vstepfrac": row["vstep_fraction"],
-            "tmax": row["time_end"],
             "nstepmax": row["nstep_max"],
             "label": row["label"],
         }
@@ -107,6 +98,14 @@ class Particle(TransientElement, abc.ABC):
 class Particle_Forward(Particle):
     element_type = "Particle Forward"
 
+    @classmethod
+    def renderer(cls) -> QgsSingleSymbolRenderer:
+        return cls.marker_renderer(color=GREY, size="3")
+
+    @classmethod
+    def renderer_output(cls) -> QgsSingleSymbolRenderer:
+        return cls.line_renderer(color=GREY, width="1.0")
+
 class Particle_Backward(Particle):
     element_type = "Particle Backward"
 
@@ -119,3 +118,11 @@ class Particle_Backward(Particle):
         data = super().process_ttim_row(row, grouped)
         data["hstepmax"] = -data["hstepmax"] # Reverse horizontal step reverses direction
         return data
+
+    @classmethod
+    def renderer(cls) -> QgsSingleSymbolRenderer:
+        return cls.marker_renderer(color=LIGHT_GREY, size="3")
+
+    @classmethod
+    def renderer_output(cls) -> QgsSingleSymbolRenderer:
+        return cls.line_renderer(color=LIGHT_GREY, width="1.0")
