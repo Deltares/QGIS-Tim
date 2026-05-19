a74a3dea56a987a485d922944223df6a09d46ec2
deltamarnix <deltamarnix@users.noreply.github.com>
2026-05-19 12:49:46 +0200

    fix enabling of qgistim plugin

--- scripts/install_qgis_tim_plugin.py
+++ scripts/install_qgis_tim_plugin.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
     plugins_path = Path(sys.argv[1]) / "python/plugins"
     qgis_path = plugins_path / "qgistim"
 
-    # Symlink qgis_env to qgis_tim, and hence qgis_env styles to ribasim styles
     plugins_path.mkdir(parents=True, exist_ok=True)
     qgis_path.unlink(missing_ok=True)
     qgis_path.symlink_to(target_path, target_is_directory=True)
-    enable_plugin("qgis_tim", sys.argv[1])
+    enable_plugin("qgistim", sys.argv[1])
