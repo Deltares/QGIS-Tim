6f615428ea9e9bc60bb6850ebb13f5df3eec7d97
JoerivanEngelen <joerivanengelen@hotmail.com>
2026-06-25 15:20:38 +0200

    chmod executable for Linux and Mac

--- .github/workflows/create_pyinstaller_zip.py
+++ .github/workflows/create_pyinstaller_zip.py
@@ -2,11 +2,20 @@ import hashlib
 import os
 import platform
 import shutil
+import stat
+from pathlib import Path
+
+# Find gistim executable in dist folder irregardless of platform and suffix.
+dist_folder = Path("./dist/gistim")
+gistim_executable = dist_folder.glob("gistim*").__next__()
+# Set execution mode for the executable in the dist folder
+# Irrelevant for Windows, but needed for Linux and MacOS.
+gistim_executable.chmod(gistim_executable.stat().st_mode | stat.S_IEXEC)
 
 # Create archive
 # Use the RUNNER_OS variable on the Github Runner. Use platform system locally.
 system = os.environ.get("RUNNER_OS", platform.system())
-zippath = shutil.make_archive(f"./dist/gistim-{system}", "zip", "./dist/gistim")
+zippath = shutil.make_archive(f"./dist/gistim-{system}", "zip", str(dist_folder))
 
 # Create a checksum
 with open(zippath, "rb", buffering=0) as f:
